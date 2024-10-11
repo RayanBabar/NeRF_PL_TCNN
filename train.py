9b8a77cd0e4bfd067bb7d42942da3591db46a216
RayanBabar <alrayanbabur@gmail.com>
2024-10-11 20:39:55 +0500

    modified train.py commit

--- train.py
+++ train.py
@@ -91,14 +91,14 @@ class NeRFSystem(LightningModule):
     def train_dataloader(self):
         return DataLoader(self.train_dataset,
                           shuffle=True,
-                          num_workers=2,
+                          num_workers=1,
                           batch_size=self.hparams.batch_size,
                           pin_memory=True)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset,
                           shuffle=False,
-                          num_workers=2,
+                          num_workers=1,
                           batch_size=1,
                           pin_memory=True)
 
